835953d93bc331001b430058a0659e32873cf88b
TieNiu-Zhao <125936733+TieNiu-Zhao@users.noreply.github.com>
2023-04-18 16:52:37 +0800

    Update smartcontract.go

--- smartcontract.go
+++ smartcontract.go
@@ -14,10 +14,10 @@ type Ciphertext struct {
 }
 
 type Proposal struct {
-	NoisyModel           []float64 	`json:"noisymodel"`			// 加噪模型 
+	NoisyModel           []float64 	`json:"noisymodel"`		// 加噪模型 
 	EncryptedModel       Ciphertext	`json:"encryptedmodel"`		// 加密模型
 	EncryptedNoisy       Ciphertext	`json:"encryptenoisy"`		// 加密噪声
-	EncryptedNoisyModel  Ciphertext `json:"encryptednoisymodel"`// 加密加噪模型
+	EncryptedNoisyModel  Ciphertext `json:"encryptednoisymodel"`	// 加密加噪模型
 }
 
 type Endorsement struct {
@@ -27,7 +27,7 @@ type Endorsement struct {
 
 type Update struct {
 	Endorsements     []Endorsement 	`json:"endorsements"` 	// 背书结果
-	EncryptedModel   ckkscipher     `json:"encryptedmodel"`	// 加密模型
+	EncryptedModel   []Ciphertext   `json:"encryptedmodel"`	// 加密模型
 }
 
 var num int 			// 通过背书的客户端数量 
@@ -75,6 +75,34 @@ func (c *Chaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	}
 
 	return shim.Error("Invalid invoke function name. Expecting \"endorsement\"")
+}
+
+func getProposal(stub shim.ChaincodeStubInterface) (Proposal, error) {
+	// 从交易参数中获取proposal
+	args := stub.GetArgs() 
+	if len(args) != 1 { 
+		return Proposal{}, errors.New("参数个数不正确") 
+	}
+	var proposal Proposal
+	err := json.Unmarshal(args[0], &proposal)
+	if err != nil {
+		return Proposal{}, errors.New("参数格式不正确")
+	}
+	return proposal, nil
+}
+
+func getUpdate(stub shim.ChaincodeStubInterface) (Update, error) {
+	// 从交易参数中获取update
+	args := stub.GetArgs()
+	if len(args) != 1 {
+		return Update{}, errors.New("参数个数不正确")
+	}
+	var update Update
+	err := json.Unmarshal(args[0], &update)
+	if err != nil {
+		return Update{}, errors.New("参数格式不正确")
+	}
+	return update, nil
 }
 
 func (c *Chaincode) Endorsement(stub shim.ChaincodeStubInterface) pb.Response {
@@ -143,11 +171,57 @@ func addCipher(cipherA, cipherB Ciphertext, q int64) Ciphertext {
 	for i := 0; i < len(cipherA.ax); i++ {
 		cipherC.ax[i] = (cipherA.ax[i] + cipherB.ax[i]) % q // 实部相加取模
 		cipherC.bx[i] = (cipherA.bx[i] + cipherB.bx[i]) % q // 虚部相加取模 
-	} 
+	}
 	return cipherC 
 }
 
-// upload()函数，用于上传密文求和结果到最新区块 
+func equal(cipherA, cipherB Ciphertext) bool {
+	for i := 0; i < len(cipherA.ax); i++ {
+		if cipherA.ax[i] != cipherB.ax[i] || cipherA.bx[i] != cipherB.bx[i] {
+			return false // 如果有一个系数不相等，返回false 
+		} 
+	}
+	return true
+}
+
+func sign(proposal Proposal) ([]byte, error) {
+	// 获取背书节点的私钥 
+	privKey, err := getPrivateKey() 
+	if err != nil {
+		return nil, err
+	}
+	// 将proposal转换为字节数组 
+	proposalBytes, err := json.Marshal(proposal)
+	if err != nil {
+		return nil, err
+	}
+	// 使用私钥对proposal进行签名 
+	signature, err := ecdsa.SignASN1(rand.Reader, privKey, proposalBytes)
+	if err != nil {
+		return nil, err
+	}
+	// 返回签名结果 
+	return signature, nil
+}
+
+func verify(endorsements []byte) bool {
+	// 获取背书节点的公钥 
+	pubKey, err := getPublicKey()
+	if err != nil {
+		return false
+	}
+	// 将endorsements分割为proposal和signature两部分 
+	proposal := endorsements[:len(endorsements)-64]
+	signature := endorsements[len(endorsements)-64:]
+	// 使用公钥对signature进行验证 
+	valid, err := ecdsa.VerifyASN1(pubKey, proposal, signature)
+	if err != nil {
+		return false
+	}
+	// 返回验证结果 
+	return valid
+}
+
 func (c *Chaincode) upload(stub shim.ChaincodeStubInterface) pb.Response {
 	update, err := getUpdate(stub)
 	if err != nil {
